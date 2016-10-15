405dd29cfeab32f790c9c0433a7a94194de11947
Volkan SERTER <volkan.serter@sahibinden.com>
2016-10-15 20:33:55 +0300

    solution revizyon

---
+++ ConsoleApp/NEF.ConsoleApp.TestApplication/Program.cs
@@ -0,0 +1,44 @@
+﻿using Autofac;
+using NEF.DataLibrary.SqlDataLayer;
+using NEF.DataLibrary.SqlDataLayer.Interfaces;
+using NEF.Library.Business;
+using NEF.Library.Business.Interfaces;
+using NEF.Library.Entities.CrmEntities;
+using NEF.Library.IocManager;
+using NEF.Library.Utility;
+using SAHIBINDEN.DataLibrary.SqlDataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NEF.Library.Entities;
+
+namespace NEF.ConsoleApp.TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IContainer container = IocContainerBuilder.GetTestIocContainer();
+
+            ILoyaltyPointBusiness loyaltyBusiness = container.Resolve<ILoyaltyPointBusiness>();
+
+            LoyaltyPoint lp = new LoyaltyPoint()
+            {
+                Name = "123",
+                ContactId = new EntityReferenceWrapper()
+                {
+                    Id = new Guid(""),
+                    LogicalName = "contact"
+                },
+                PointType = LoyaltyPoint.PointTypeCode.CARD.ToOptionSetValueWrapper()
+            };
+
+
+            Guid id = loyaltyBusiness.Insert(lp);
+
+
+        }
+    }
+}
---
+++ ConsoleApp/NEF.ConsoleApp.TestApplication/test1.cs
@@ -0,0 +1,27 @@
+﻿using NEF.Library.Business.Interfaces;
+using NEF.Library.Entities.CrmEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NEF.ConsoleApp.TestApplication
+{
+    public class test1
+    {
+        private ILoyaltyPointBusiness _loyaltyBusiness;
+
+        public test1(ILoyaltyPointBusiness loyaltyBusiness)
+        {
+            _loyaltyBusiness = loyaltyBusiness;
+        }
+
+        public void DoWork()
+        {
+            Console.WriteLine("DoWork started...");
+
+            LoyaltyPoint loyaltyPoint = _loyaltyBusiness.Get(new Guid(""));
+
+        }
+    }
+}
