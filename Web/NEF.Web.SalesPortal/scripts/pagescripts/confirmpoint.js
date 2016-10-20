4310cb2e4af30af688fb670b44af48941829e7a6
burak.yilmaz <burak.yilmaz@etstur.com>
2016-10-21 01:25:16 +0300

    eksik dosyalar

---
+++ Web/NEF.Web.SalesPortal/scripts/pagescripts/confirmpoint.js
@@ -0,0 +1,192 @@
+﻿/// <reference path="global.js" />
+/// <reference path="index.js" />
+
+
+var ConfirmPointHelper = {
+    "OnLoad": function () {
+
+        ConfirmPointHelper.OnClickEvents();
+        ConfirmPointHelper.OnChangeEvents();
+        ConfirmPointHelper.OnChangeKeyPressEvents();
+
+        $(window).resize(function () {
+            parent.IndexHelper.AutoResize("ifrmContent");
+        });
+
+        parent.IndexHelper.AutoResize("ifrmContent");
+
+    },
+    "GetPointDetail": function (callbackFunction) {
+
+        var jData = {};
+        jData.loyaltyPointId = ConfirmPointHelper.PointId;
+        var jSonData = JSON.stringify(jData);
+
+        $.ajax({
+            url: CustomServiceUrl + "/GetLoyaltyPoint",
+            async: true,
+            dataType: "json",
+            contentType: "application/json;",
+            type: "POST",
+            data: jSonData,
+            beforeSend: function () {
+                Metronic.blockUI({ target: $("pnlQuoteDetail"), iconOnly: false, boxed: true, message: "Puan bilgileri getiriliyor..." });
+            },
+            complete: function () {
+                Metronic.unblockUI($(".page-content"));
+            },
+            success: function (data) {
+                debugger;
+                if (data != null) {
+                    if (data.Success == true)
+                        callbackFunction(data);
+                    else {
+                        parent.IndexHelper.ToastrShow(false, data.Result, "Puan Bilgileri");
+                        parent.parent.IndexHelper.CloseDialog();
+                    }
+
+                }
+                else {
+                    parent.IndexHelper.ToastrShow(false, "Beklenmedik bir hata ile karşılaşıldı!", "Puan Bilgileri");
+                    parent.parent.IndexHelper.CloseDialog();
+                }
+            },
+            error: function (a, b, c) {
+                parent.parent.IndexHelper.ToastrShow(false, "Beklenmedik bir hata ile karşılaşıldı! " + c, "Puan Bilgileri");
+            }
+        });
+    },
+    "OnClickEvents": function () {
+
+    },
+    "OnChangeKeyPressEvents": function () {
+
+    },
+    "OnChangeEvents": function () {
+
+    },
+    "SendToConfirm": function (callbackfunction) {
+
+        var jData = {};
+        jData.loyaltyPointId = ConfirmPointHelper.PointId;
+        var jSonData = JSON.stringify(jData);
+
+        $.ajax({
+            url: CustomServiceUrl + "/ConfirmPointUsage",
+            async: true,
+            dataType: "json",
+            contentType: "application/json;",
+            type: "POST",
+            data: jSonData,
+            beforeSend: function () {
+                Metronic.blockUI({ target: $("#main"), iconOnly: false, boxed: true, message: "Puan onaylanıyor..." });
+            },
+            complete: function () {
+                Metronic.unblockUI($("#main"));
+            },
+            success: function (data) {
+                callbackfunction(data);
+            },
+            error: function (a, b, c) {
+                parent.IndexHelper.ToastrShow(false, "Beklenmedik bir hata ile karşılaşıldı! " + c, "Puan Bilgileri");
+            }
+        });
+    },
+    "SendToRefuse": function (callbackfunction) {
+
+        var jData = {};
+        jData.loyaltyPointId = ConfirmPointHelper.PointId;
+        var jSonData = JSON.stringify(jData);
+
+        $.ajax({
+            url: CustomServiceUrl + "/RefusePointUsage",
+            async: true,
+            dataType: "json",
+            contentType: "application/json;",
+            type: "POST",
+            data: jSonData,
+            beforeSend: function () {
+                Metronic.blockUI({ target: $("#main"), iconOnly: false, boxed: true, message: "Puan reddediliyor..." });
+            },
+            complete: function () {
+                Metronic.unblockUI($("#main"));
+            },
+            success: function (data) {
+                callbackfunction(data);
+            },
+            error: function (a, b, c) {
+                parent.IndexHelper.ToastrShow(false, "Beklenmedik bir hata ile karşılaşıldı! " + c, "Puan Bilgileri");
+            }
+        });
+    },
+    "ToastrShow": function (result, message, title) {
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "positionClass": "toast-top-right",
+            "onclick": null,
+            "showDuration": "1000",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+
+        if (result) {
+            toastr["success"](message, title);
+        }
+        else {
+            toastr["error"](message, title);
+        }
+
+    },
+    "PointId": ""
+}
+
+function mainController($scope) {
+
+    var url = $.url(document.location);
+    ConfirmPointHelper.PointId = url.param("pointid");
+
+    $scope.IsDisabled = false;
+
+    ConfirmPointHelper.GetPointDetail(function (e) {
+        $scope.$apply(function () {
+            $scope.point = e.ReturnObject;
+        });
+
+    });
+
+    $scope.SendToConfirm = function () {
+        ConfirmPointHelper.SendToConfirm(function (e) {
+            $scope.$apply(function () {
+                if (e.Success) {
+                    $scope.IsDisabled = true;
+                    parent.IndexHelper.ToastrShow(true, e.Result, "Onay İşlemleri");
+                    parent.window.close();
+                }
+                else {
+                    parent.IndexHelper.ToastrShow(false, e.Result, "Onay İşlemleri");
+                }
+            });
+        });
+    };
+
+    $scope.SendToRefuse = function () {
+        ConfirmPointHelper.SendToRefuse(function (e) {
+            $scope.$apply(function () {
+                if (e.Success) {
+                    $scope.IsDisabled = true;
+                    parent.IndexHelper.ToastrShow(true, e.Result, "Onay İşlemleri");
+                    parent.window.close();
+                }
+                else {
+                    parent.IndexHelper.ToastrShow(false, e.Result, "Onay İşlemleri");
+                }
+            });
+        });
+    };
+}
---
+++ Web/NEF.Web.SalesPortal/scripts/pagescripts/contactSearchForSale.js
@@ -0,0 +1,149 @@
+﻿/// <reference path="global.js" />
+/// <reference path="index.js" />
+
+
+var ContactSearchForSaleHelper = {
+    "OnLoad": function () {
+        Metronic.init(); // init metronic core componets
+        Layout.init(); // init layout
+        //QuickSidebar.init() // init quick sidebar
+        //Index.init();
+        //Index.initDashboardDaterange();
+        //Index.initJQVMAP(); // init index page's custom scripts
+        //Index.initCalendar(); // init index page's custom scripts
+        //Index.initCharts(); // init index page's custom scripts
+        //Index.initChat();
+        //Index.initMiniCharts();
+        ////Index.initIntro();
+        //Tasks.initDashboardWidget();
+
+        $(".page-title", parent.document).html("Müşteri Arama");
+        $("#btnBreadPage", parent.document).html("Müşteri Arama");
+
+        $("#liSearch", parent.document).addClass("active");
+        $("#liHome", parent.document).removeClass("active");
+        $("#liHouseSearch", parent.document).removeClass("active");
+        $("#liAllChart", parent.document).removeClass("active");
+
+        ContactSearchForSaleHelper.OnClickEvents();
+        ContactSearchForSaleHelper.OnChangeEvents();
+        ContactSearchForSaleHelper.OnChangeKeyPressEvents();
+
+    },
+    "MakeSearch": function (callbackFunction) {
+        var jData = {};
+        jData.searchedWord = $("#txtKey").val();
+        var jSonData = JSON.stringify(jData);
+
+        if (jData.searchedWord != "") {
+            $.ajax({
+                url: CustomServiceUrl + "/MakeContactSearch",
+                async: true,
+                dataType: "json",
+                contentType: "application/json;",
+                type: "POST",
+                data: jSonData,
+                beforeSend: function () {
+                    Metronic.blockUI({ target: $("#pcBody"), iconOnly: true });
+                },
+                complete: function () {
+                    Metronic.unblockUI($("#pcBody"));
+                },
+                success: function (data) {
+                    if (data != null) {
+                        data = JSON.parse(data);
+                        callbackFunction(data);
+                    }
+                    else {
+                        parent.IndexHelper.ToastrShow(false, "Beklenmedik bir hata ile karşılaşıldı!", "Müşteri Arama");
+                    }
+                },
+                error: function (a, b, c) {
+                    parent.IndexHelper.ToastrShow(false, "Beklenmedik bir hata ile karşılaşıldı! " + c, "Müşteri Arama");
+                }
+            });
+        }
+        else {
+            parent.IndexHelper.ToastrShow(false, "Lütfen aranacak bilgiyi yazınız!", "Müşteri Arama");
+        }
+
+    },
+    "CustomerHasCallCenterCall": function (contactId, callbackFunction) {
+        var jData = {};
+        jData.customerId = contactId;
+        var jSonData = JSON.stringify(jData);
+
+        $.ajax({
+            url: CustomServiceUrl + "/CustomerHasCallCenterCall",
+            async: true,
+            dataType: "json",
+            contentType: "application/json;",
+            type: "POST",
+            data: jSonData,
+            beforeSend: function () {
+                Metronic.blockUI({ target: $("#pcBody"), iconOnly: true });
+            },
+            complete: function () {
+                Metronic.unblockUI($("#pcBody"));
+            },
+            success: function (data) {
+                if (data != null) {
+                    callbackFunction(data);
+                }
+                else {
+                    parent.IndexHelper.ToastrShow(false, "Beklenmedik bir hata ile karşılaşıldı!", "Müşteri Arama");
+                }
+            },
+            error: function (a, b, c) {
+                parent.IndexHelper.ToastrShow(false, "Beklenmedik bir hata ile karşılaşıldı! " + c, "Müşteri Arama");
+            }
+        });
+
+    },
+    "OnClickEvents": function () {
+
+    },
+    "OnChangeKeyPressEvents": function () {
+        //$("#txtKey").keydown(function (e) {
+        //    if (e.which == 13) {
+        //        $scope.makeSearch();
+        //    }
+        //});
+    },
+    "OnChangeEvents": function () {
+
+    }
+}
+
+function mainController($scope) {
+    $scope.isVisible = false;
+    $scope.makeSearch = function ($keyEvent) {
+        if ($keyEvent == null || $keyEvent.which == 13) {
+
+            ContactSearchForSaleHelper.MakeSearch(function (e) {
+                $scope.$apply(function () {
+                    $scope.isNewContact = true;
+                    if (e.Success) {
+                        $scope.searchResults = e.ReturnObject;
+                        $scope.isVisible = true;
+
+                    }
+                    else {
+                        $scope.isVisible = false;
+                    }
+
+                });
+            });
+        }
+    };
+
+    $scope.contactTypes = GlobalHelper.ContactTypes;
+    $scope.contactTypeClass = GlobalHelper.ContactTypeClass;
+
+    $scope.selectContact = function (id, name, type) {
+
+        $("#txtContact", parent.document).val(name).attr("crmid", id);
+        dialog.modal('hide');
+        //parent.IndexHelper.CloseDialog();
+    }
+}
